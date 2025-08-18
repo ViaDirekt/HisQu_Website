05e02945ad4c2d18c1094f9820fcad0a5eddcf31
Friedrich Answin Daniel Motz <hello@danielmotz.de>
2025-08-18 11:08:48 +0200

    Ein bisschen aufgeräumt, sollte jetzt passen

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+import astro from "./astro.svg";
+import background from "./background.svg";
+import dfgLogoSvg from "./dfg_logo_englisch_blau_en_4c.svg";
+import forschungsdatenzyklus from "./forschungsdatenzyklus.drawio.png";
+import imageWebp from "./image.vH2ZM2ku_3g5zp.webp";
+import logo from "./logo.png";
+import starlogPlaceholder from "./starlog-placeholder-2.jpg";
+import team from "./team.jpg";
+
+export {
+    astro,
+    background,
+    dfgLogoSvg,
+    forschungsdatenzyklus,
+    imageWebp,
+    logo,
+    starlogPlaceholder,
+    team,
+};
