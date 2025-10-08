42eecc226e18c66406b3302119f77a5a4732bab2
XXios <noasulimannn@gmail.com>
2025-10-08 16:42:31 +0200

    First changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hisqu.de',
+  site: "https://hisqu.de",
+
+  integrations: [icon()],
 
   i18n: {
-      locales: ["en"],       // add as many as you need
-      defaultLocale: "en",
-      routing: { prefixDefaultLocale: false }, // "/about" (EN) and "/de/about"
-	},
+    locales: ["en"], // add as many as you need
+    defaultLocale: "en",
+    routing: { prefixDefaultLocale: false }, // "/about" (EN) and "/de/about"
+  },
 
   vite: {
     plugins: [tailwindcss()],
